a7c2548da2423cf80081ee23dad7fe357a9d9da4
Abigail2904 <galilearodriguez867@gmail.com>
2025-12-05 14:12:46 -0400

    cambios

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const app = require('./app');
 const http = require('http');
 
-
 const server = http.createServer(app);
 
-server.listen(3003, () => {
+// Render asigna el puerto en process.env.PORT
+const PORT = process.env.PORT || 3003;
 
-    console.log('El servidor esta corriendo en el puerto 3003');
+server.listen(PORT, () => {
+  console.log(`El servidor está corriendo en el puerto ${PORT}`);
 });
